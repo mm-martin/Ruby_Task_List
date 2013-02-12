24e144ea7793af06c041e1244f0300e66d41363c
Martin Lynagh <martin@mediamolecule.com>
2013-02-12 11:03:22 +0000

    changed DB Schema
    need to fix display layer

--- src/recall.rb
+++ src/recall.rb
@@ -8,65 +8,90 @@ class Task
 	puts "Creating task class"
 	include DataMapper::Resource
 	property :id, Serial
-	property :content, Text, :required => true
-	property :complete, Boolean, :required => true, :default => 0
+	property :summary, Text, :required => true
+	property :status, String, :required => true
+	property :rank, Integer
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
 
 class Status
-	puts "Creating stat class"
+	puts "Creating status class"
 	include DataMapper::Resource
 	property :id, Serial
 	property :status, String
-	property :description, Text 	
 end
 
-DataMapper.auto_upgrade!
+DataMapper.auto_migrate!
+
+status_names = ['To Do', 'In Progress', 'Complete']
+status_names.each do |status_name|
+	puts "adding " + status_name
+	s = Status.new
+	s.status = status_name
+end
+
+# getBottomRank do
+# 	# to do: sort by rank ascending and return the last value
+# 	return 0
+# end
 
 get '/' do
+	puts "get"
 	@tasks = Task.all :order => :id.desc
+	puts "1"
 	@title = 'All Tasks'
+	puts "2"
 	erb :home
+	puts "3"
 end
 
 post '/' do
-	n = Task.new
-	n.content = params[:content]
-	n.created_at = Time.now
-	n.updated_at = Time.now
-	n.save
+	puts "post"
+	t = Task.new
+	t.summary = params[:summary]
+	t.status = 'To Do'
+	t.rank = 0
+	t.created_at = Time.now
+	t.updated_at = Time.now
+	t.save
 	redirect '/'
 end
 
 get '/:id' do
+	puts "get id"
 	@task = Task.get params[:id]
 	@title = "Edit task ##{params[:id]}"
 	erb :edit
 end
 
 put '/:id' do
-	n = Task.get params[:id]
-	n.content = params[:content]
-	n.complete = params[:complete] ? 1 : 0
-	n.updated_at = Time.now
-	n.save
+	puts "put id"
+	t = Task.get params[:id]
+	t.summary = params[:summary]
+	t.status = params[:status]
+	t.rank = params[:rank]
+	t.updated_at = Time.now
+	t.save
 	redirect '/'
 end
 
 get '/:id/delete' do
+	puts "get id delete"
 	@task = Task.get params[:id]
 	@title = "Confirm deletion of task ##{params[:id]}"
 	erb :delete
 end
 
 delete '/:id' do
-	n = Task.get params[:id]
-	n.destroy
+	puts "delete id"
+	t = Task.get params[:id]
+	t.destroy
 	redirect '/'
 end
 
 get '/:id/complete' do
+	puts "complete"
 	n = Task.get params[:id]
 	n.complete = n.complete ? 0 : 1 # flip it
 	n.updated_at = Time.now
